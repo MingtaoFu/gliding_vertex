1427809c38a1c479d527dfa5e70ecb591c29d6c8
Mingtao Fu <madebyrag@gmail.com>
2019-12-27 00:16:15 +0800

    fix bugs

--- maskrcnn_benchmark/csrc/vision.cpp
+++ maskrcnn_benchmark/csrc/vision.cpp
@@ -1,6 +1,5 @@
 // Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 #include "nms.h"
-#include "coveredby.h"
 #include "ROIAlign.h"
 #include "PSROIAlign.h"
 #include "ROIPool.h"
@@ -10,7 +9,6 @@
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("nms", &nms, "non-maximum suppression");
-  m.def("coveredby", &coveredby, "nms variant");
   m.def("roi_align_forward", &ROIAlign_forward, "ROIAlign_forward");
   m.def("roi_align_backward", &ROIAlign_backward, "ROIAlign_backward");
   m.def("psroi_align_forward", &PSROIAlign_forward, "PSROIAlign_forward");
